f00336fc945159494f3207270bf036480d0b4a91
Bhavesh <bhavesh189kumar@gmail.com>
2021-03-21 13:40:55 +0530

    Connected to mysql database

--- FileSharingApplication/FileSharingApplication/settings.py
+++ FileSharingApplication/FileSharingApplication/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'FileSharingApplication.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Rocketshare',
+        'PORT': '3306',
+        'USER': 'bhavesh',
+        'PASSWORD': 'bhavesh@2002',
+        'HOST': 'localhost',
     }
 }
 
