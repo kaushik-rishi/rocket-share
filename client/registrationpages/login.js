fa48a157b6dabf9377b74e77be325d57fe53f25d
kaushik-rishi <rishi.cp01@gmail.com>
2021-03-25 22:03:05 +0530

    [TEMPORARY] Imported the getPayload and alerted the user login message
    This is done temporarily and will be revoked asap.

--- client/registrationpages/login.js
+++ client/registrationpages/login.js
@@ -1,6 +1,21 @@
 const loginForm = document.querySelector('form#login_form')
 const errorEl = document.querySelector('span#login_form__error')
 
+function getAuthToken() {
+    var arrayb = document.cookie.split(";");
+    for (const item of arrayb) {
+        if (item.startsWith("authtoken=")) {
+            return item.substr(10);
+        }
+    }
+}
+
+function getPayload(token) {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(atob(base64));
+}
+
 loginForm.addEventListener('submit', function (e) {
     e.preventDefault()
 
@@ -19,6 +34,7 @@ loginForm.addEventListener('submit', function (e) {
         .then(response => response.json())
         .then(json => {
             if (json.ok === true) {
+                alert(`Hello Again 👋 ${getPayload(getAuthToken()).name}`)
                 window.location = '/share'
             } else {
                 errorEl.innerText = json.err
--- client/registrationpages/signup.js
+++ client/registrationpages/signup.js
@@ -1,6 +1,21 @@
 const signupForm = document.querySelector('form#signup_form')
 const errorEl = document.querySelector('span#signup_form__error')
 
+function getAuthToken() {
+    var arrayb = document.cookie.split(";");
+    for (const item of arrayb) {
+        if (item.startsWith("authtoken=")) {
+            return item.substr(10);
+        }
+    }
+}
+
+function getPayload(token) {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(atob(base64));
+}
+
 signupForm.addEventListener('submit', function (e) {
     e.preventDefault()
     
@@ -13,15 +28,19 @@ signupForm.addEventListener('submit', function (e) {
     fetch('/register', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
         },
         body: JSON.stringify(user)
     })
         .then(response => response.json())
         .then(json => {
             console.log(json)
+
             if (json.ok === true) {
-                window.location = '/share'
+                errorEl.innerText = ''
+                alert(`Welcome ${getPayload(getAuthToken()).name} Please consider logging in 😁`)
+                window.location = '/registrationpages/login.html'
             } else {
                 errorEl.innerText = json.err
             }
