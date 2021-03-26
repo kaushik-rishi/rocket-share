6caa6bf41c16729f1b8e9a31f9e79c58e1d45de8
kaushik-rishi <rishi.cp01@gmail.com>
2021-03-26 16:13:46 +0530

    Temp commit

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -4,16 +4,10 @@ const { uploadMiddleware } = require('../middleware/uploadMulterMiddleware')
 const singleUpload = uploadMiddleware.single('fileupload')
 
 router.post('/upload', (req, res, err) => {
-    const file = req.file
-
-    if (!file) 
-        return res.status(400).send({
-            ok: false,
-            err: 'Something went wrong while uplaoding the file...'
-        })
-    
-
+    // store the file
     singleUpload(req, res, (err) => {
+
+        // error while uploading the file
         if (err) {
             return res.status(500).send({
                 ok: false,
@@ -21,7 +15,26 @@ router.post('/upload', (req, res, err) => {
             })
         }
 
-        
+        const file = req.file
+
+        if (!file) {
+            // bad request
+            res.status(400).json({
+                "ok": false,
+                "err": "Something went wrong while uplaoding the file... [The fields should not be empty]"
+            })
+            
+        } else {
+            // save the upload metadata to the server
+            
+
+            // send success reponse
+            res.status(500).json({
+                ok: true,
+                msg: 'file uploaded successfully',
+                file
+            })
+        }
     })
 })
 
