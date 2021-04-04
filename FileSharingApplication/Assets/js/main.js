3e578d74535f871466fc3cd8aa5144b4cd71f4a4
kaushik-rishi <rishi.cp01@gmail.com>
2021-04-04 14:13:57 +0530

    Remove Legacy Django Code

--- FileSharingApplication/Assets/js/main.js
+++
@@ -1,6 +0,0 @@
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
-  })
-);
